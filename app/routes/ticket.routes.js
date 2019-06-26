b230cf31e4f448a29cfb6c6051045511a3738c70
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-06-26 12:04:35 +0700

    Add ticket routes

--- app/routes/ticket.routes.js
+++ app/routes/ticket.routes.js
@@ -3,4 +3,7 @@ const ticketController = require('../controllers/ticket.controller.js');
 module.exports = (app) => {
     app.post('/tickets', ticketController.create);
     app.get('/tickets', ticketController.findAll);
+    app.get('/tickets/:id', ticketController.findOne);
+    app.put('/tickets/:id', ticketController.update);
+    app.delete('/tickets/:id', ticketController.delete);
 };
