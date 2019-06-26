5e9812e7631c4a5357133446e45898c30de831b8
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-06-26 12:00:18 +0700

    Add ticket controller

--- app/controllers/ticket.controller.js
+++ app/controllers/ticket.controller.js
@@ -2,17 +2,104 @@ const Ticket = require("../models/ticket.model.js");
 
 module.exports = {
     create: (req, res) => {
-        let ticket = new Ticket({
+        let ticket = new Order({
             arrival: req.body.arrival,
             destination: req.body.destination,
-            price: req.body.price
+            price: req.body.price,
+            numberOfInfant: req.body.numberOfInfant
         });
-        ticket.save();
-        res.send(ticket);
+        ticket
+            .save()
+            .then(data => {
+                res.send(data);
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: err.message || "Some error occurred while creating the ticket."
+                });
+            });
     },
+
     findAll: (req, res) => {
-        Ticket.find().then(data => {
-            res.send(data)
+        Ticket.find()
+            .then(data => {
+                if (!data) {
+                    res.send({
+                        message: `Ticket not found with id ${req.params.id}`
+                    });
+                }
+                res.send(data);
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: err.message || "Some error occurred while retrieving the Ticket."
+                });
+            });
+    },
+    findOne: (req, res) => {
+        Ticket.findById(req.params.id)
+            .then(data => {
+                if (!data) {
+                    res.status(404).send({
+                        message: `Ticket not found with id ${req.params.id}`
+                    });
+                }
+                res.send(data);
+            })
+            .catch(err => {
+                if (err.kind === 'ObjectId') {
+                    return res.status(404).send({
+                        message: `Ticket not found with id ${req.params.id}`
+                    });
+                }
+                return res.status(500).send({
+                    message: `Error retrieving Order with id ${req.params.id}`
+                });
+            });
+    },
+    update: (req, res) => {
+        Ticket.findByIdAndUpdate(req.params.id, {
+            arrival: req.body.arrival,
+            destination: req.body.destination,
+            price: req.body.price,
+            numberOfInfant: req.body.numberOfInfant
+        }).then((data) => {
+            if (!data) {
+                return res.status(404).send({
+                    message: `Ticket not found with id ${req.params.id}`
+                });
+            }
+            res.send(data);
+        }).catch((err) => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: `Ticket not found with id ${req.params.id}`
+                });
+            }
+            return res.status(500).send({
+                message: `Error updating Ticket with id ${req.params.id}`
+            });
+        });
+    },
+    delete: (req, res) => {
+        Ticket.findOneAndDelete(req.params.id).then(data => {
+            if (!data) {
+                return res.status(404).send({
+                    message: `Ticket not found with id ${req.params.id}`
+                });
+            }
+            res.send({
+                message: "Ticket deleted successfully!"
+            });
+        }).catch(err => {
+            if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+                return res.status(404).send({
+                    message: `Ticket not found with id ${req.params.id}`
+                });
+            }
+            return res.status(500).send({
+                message: `Could not delete note with id ${req.params.id}`
+            });
         });
     }
 };
